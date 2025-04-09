c5693808914a73b09ccc579ed2f2f793cc48a5b0
Jeff Tan <JeffTan.b@gmail.com>
2025-04-08 22:06:57 -0700

    init hw1 folders

--- CS142-S25-Problem-Solving-HW1/E-Disneyland/main.cpp
+++ CS142-S25-Problem-Solving-HW1/E-Disneyland/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-using namespace std;
 
-int main(){
-    cout << "Hello, World!" << endl;
-    return 0;
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(0);
+
+
+
+
+
+
+  return 0;
 }
