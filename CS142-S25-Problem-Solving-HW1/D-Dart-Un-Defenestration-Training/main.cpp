0649da7e73dfef138bb7a0c5b3ffaada4979b31a
Jeff Tan <JeffTan.b@gmail.com>
2025-05-01 16:03:03 -0700

    0.3 final

--- CS142-S25-Problem-Solving-HW1/D-Dart-Un-Defenestration-Training/main.cpp
+++ CS142-S25-Problem-Solving-HW1/D-Dart-Un-Defenestration-Training/main.cpp
@@ -13,7 +13,12 @@ int main()
 
   std::vector<int> darts_thrown;
 
-  bool found_optimal = false;
+  bool found_subtractable = false;
+
+  std::vector<int> result_points(4, 0);
+
+
+
 
   std::cin >> num_sectors;
   std::cin >> full_score;
@@ -21,44 +26,55 @@ int main()
   // point-sector pairs
   std::vector<std::pair<int, int>> point_sectors(num_sectors);
 
-  for ( std::size_t i = 0; i < num_sectors; ++ i )
+  for ( std::size_t i = 0; i < num_sectors; ++i )
   {
     std::cin >> point_sectors[i].first;
     point_sectors[i].second = static_cast<int>(i + 1);
   }
 
-  std::sort(point_sectors.begin(), point_sectors.end(), std::greater<>());
+  // sort each point sector based on highest score pair first which should reduce leonard's score faster?
+  std::sort(point_sectors.begin(), point_sectors.end(), std::greater_equal<>());
 
-  std::vector<int> result_points(4, 0);
 
+
+
+
+
+  // each dart find sector without going into negative
+  // if subtractable, then reduce the score and add sector index
   current_score = full_score;
 
   for ( std::size_t i = 0; i < 4; ++i )
   {
-    found_optimal = false;
+    found_subtractable = false;
 
     for ( const auto &sector : point_sectors )
     {
-      if ( current_score >= sector.first)
+      if ( current_score >= sector.first || current_score == 0)
       {
         current_score -= sector.first;
         result_points[i] = sector.second;
-        found_optimal = true;
+        found_subtractable = true;
+
+
 
         break;
       }
     }
 
-    if ( !found_optimal )
+
+    if ( current_score < 0 )
+    {
+      current_score = full_score;
+    }
+
+    if ( !found_subtractable )
     {
       result_points[i] = 0;
     }
-
-    if ( current_score < 0 )
-    {//
-      current_score = full_score;
-    }
   }
+
+
 
   for ( std::size_t i = 0; i < 4; ++i )
   {
