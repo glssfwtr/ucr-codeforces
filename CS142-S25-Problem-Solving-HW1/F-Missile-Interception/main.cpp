c22d47fbca3d107a25e0e79485cbf8ea05214818
Jeff Tan <JeffTan.b@gmail.com>
2025-05-01 23:27:31 -0700

    Finish 1F

--- CS142-S25-Problem-Solving-HW1/F-Missile-Interception/main.cpp
+++ CS142-S25-Problem-Solving-HW1/F-Missile-Interception/main.cpp
@@ -2,17 +2,23 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
-using namespace std;
 
-double DistanceSquared(double x1, double y1, double x2, double y2)
+int DistanceSquared(int x1, int y1, int x2, int y2)
 {
   return ( ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)) );
 }
 
+
 int main()
 {
-  int n;
   int x1, y1, x2, y2;
+  std::size_t n;
+
+  std::vector<std::pair<int, int>> distance_pairs;
+
+  int m_x;
+  int m_y;
+
 
   std::cin >> x1;
   std::cin >> y1;
@@ -21,63 +27,55 @@ int main()
 
   std::cin >> n;
 
-  std::vector<std::pair<int, int>> missiles(n);
-  std::vector<double> dist1(n);
-  std::vector<double> dist2(n);
+  int result = INT_MAX;
 
+  // get all squared distances to both system coordinates
   for ( std::size_t i = 0; i < n; ++i )
   {
-    std::cin >> missiles[i].first >> missiles[i].second;
+    std::cin >> m_x;
+    std::cin >> m_y;
+
+    int distance1 = DistanceSquared(x1, y1, m_x, m_y);
+    int distance2 = DistanceSquared(x2, y2, m_x, m_y);
+
+    distance_pairs.push_back({distance1, distance2});
   }
 
+  // sort distance based on first system's pair.first increasing order
+  std::sort(distance_pairs.begin(), distance_pairs.end());
 
 
 
 
 
 
+  // dp[i] = min_radius^2 for system 2 to cover all missiles
+  std::vector<int> dp_table(n + 1, 0);
 
-  // precompute distances each missile to both systems
+  // the min of radius^2 needed to cover every missile
+  // min radius^2 is max of radius^2 needed for next to last missile
+  // let min radius determined by farthest missile instead
+  for ( int i = n - 1; i >= 0; --i )
+  {
+    dp_table[i] = std::max(dp_table[i + 1], distance_pairs[i].second);
+  }
+
+  result = std::min(result, dp_table[0]);
+
+
+  // find optimal, minimal point by going through dp table current index and dp + 1
   for ( std::size_t i = 0; i < n; ++i )
   {
-    dist1[i] = DistanceSquared(x1, y1, missiles[i].first, missiles[i].second);
-    dist2[i] = DistanceSquared(x2, y2, missiles[i].first, missiles[i].second);
+    int r1_sqr = distance_pairs[i].first;
+    int r2_sqr = dp_table[i + 1];
+
+    result = std::min(result, r1_sqr + r2_sqr);
   }
 
-  // deprecated but fk it we ball
-  double result = LLONG_MAX;
+  result = std::min(result, distance_pairs[n - 1].first);
 
-  // try all pairs of missile radius
-  for ( int i = -1; i < n; ++i )
-  {
-    for ( int j = -1; j < n; ++j )
-    {
-      // set radius based on chosen missiles
-      // -1 represents using radius 0 (not covering any missiles)
-      double r1_squared = (i == -1) ? 0 : dist1[i];
-      double r2_squared = (j == -1) ? 0 : dist2[j];
+  std::cout << result;
 
-      // check if all missiles covered with these radius
-      bool all_covered = true;
-
-      for ( int k = 0; k < n; ++k )
-      {
-        if ( dist1[k] > r1_squared && dist2[k] > r2_squared )
-        {
-          all_covered = false;
-          break;
-        }
-      }
-
-
-      if ( all_covered )
-      {
-        result = std::min(result, r1_squared + r2_squared);
-      }
-    }
-  }
-
-  cout << result << "\n";
 
   return 0;
 }
